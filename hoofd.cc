4f6458efb883c204ed06952af2e191f76898f159
TeunVooijs <teunvooys@gmail.com>
2021-11-30 22:56:40 +0100

    Eerste versie

---
+++ hoofd.cc
@@ -0,0 +1,11 @@
+// file hoofd.cc
+#include <iostream>
+#include "koffiebord.h"
+using namespace std;
+
+int main ( ) {
+  koffiebord bla;
+  bla.drukaf();
+  cout << "werkt niet" << endl;
+  return 0;
+}//main
---
+++ koffiebord.cc
@@ -0,0 +1,28 @@
+// file koffiebord.cc
+#include <iostream>
+#include "koffiebord.h"
+using namespace std;
+
+int leesgetal (int bovengrens) {
+  cout << bovengrens << endl;
+  return 0;
+}//leesgetal
+
+bordvakje::bordvakje ( ) {
+  // TODO
+}//bordvakje::bordvakje
+
+koffiebord::koffiebord ( ) {
+  
+}//koffiebord::koffiebord
+
+koffiebord::~koffiebord ( ) {
+  // TODO
+}//koffiebord::~koffiebord
+
+void koffiebord::drukaf ( ) {
+  cout << "Een koffiebord ziet er mooi uit." << endl;
+  // TODO
+}//koffiebord::drukaf
+
+// TODO
